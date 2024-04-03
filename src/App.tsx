b0f63a26a53587359c1be2632cf28b57dd8aa4da
Magnus Westhofen <m.westhofen@12-05.de>
2024-04-03 21:38:11 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,58 +2,134 @@ import { useState } from "react";
 
 import styles from "./App.module.css";
 
+type GPTMessage = {
+  role: "user" | "bot";
+  content: string;
+};
+
 function App() {
-  const [prompt, setPrompt] = useState<string>("");
+  const [person, setPerson] = useState<string>("");
+  const [messages, setMessages] = useState<GPTMessage[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [image, setImage] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const [currentQuestion, setCurrentQuestion] = useState<string>("");
 
   const submit = async (e: React.FormEvent<HTMLFormElement>) => {
     setIsLoading(true);
     e.preventDefault();
     try {
-      const response = await fetch("https://gpt.westhofen.me/gpainter.php", {
+      const response = await fetch("https://gpt.westhofen.me/werbinich.php", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ prompt }),
+        body: JSON.stringify({ messages }),
       });
       const data = await response.text();
-      console.log(data);
-
       setIsLoading(false);
-      return setImage(data);
+      return setCurrentQuestion(data);
     } catch (e) {
       setIsLoading(false);
       setError("Ein Fehler ist aufgetreten.");
     }
   };
 
+  const answerQuestion = async (text: string) => {
+    setIsLoading(true);
+    const newMessages = [
+      ...messages,
+      { role: "assistant", content: currentQuestion },
+      { role: "user", content: text },
+    ] as GPTMessage[];
+    setMessages(newMessages);
+    try {
+      const response = await fetch("https://gpt.westhofen.me/werbinich.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages: newMessages }),
+      });
+      const data = await response.text();
+      setIsLoading(false);
+      return setCurrentQuestion(data);
+    } catch (e) {
+      setIsLoading(false);
+      setError("Ein Fehler ist aufgetreten.");
+    }
+  };
+  if (currentQuestion?.includes("Danke")) {
+    return (
+      <div className={styles.app}>
+        <div className={styles.layout} style={{ textAlign: "center" }}>
+          <h1 className={styles.question}>Danke fürs Spielen!</h1>
+          <button
+            type="button"
+            className={styles.button}
+            onClick={() => window.location.reload()}
+          >
+            Nochmal!
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className={styles.app}>
-      <div className={styles.layout}>
-        <div
-          className={styles.canvas}
-          dangerouslySetInnerHTML={{ __html: image }}
-        ></div>
-
+      <div className={styles.container}>
+        <h1 className={styles.headline}>GPT Wer bin ich</h1>
         <form onSubmit={submit} className={styles.formElement}>
-          <label className={styles.label} htmlFor="input">
-            Was möchtest du zeichnen?
-          </label>
-          <input
-            id="input"
-            className={styles.input}
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-            type="text"
-          />
-          <button className={styles.button} type="submit">
-            {isLoading ? "Zeichnet..." : "Los geht's"}
-          </button>
+          {currentQuestion ? (
+            <div>
+              <div className={styles.question}>{currentQuestion}</div>
+              {isLoading ? (
+                <div className={styles.think}>Denkt nach...</div>
+              ) : (
+                <div className={styles.buttonGroup}>
+                  <button
+                    className={styles.button}
+                    type="button"
+                    onClick={() => answerQuestion("Nein")}
+                  >
+                    Nööö
+                  </button>
+
+                  <button
+                    className={styles.button}
+                    type="button"
+                    onClick={() => answerQuestion("Ja")}
+                  >
+                    Jupp
+                  </button>
+                </div>
+              )}
+            </div>
+          ) : (
+            <>
+              <label className={styles.label} htmlFor="input">
+                Welche berühmte Persönlichkeit bist du?
+              </label>
+              <input
+                id="input"
+                className={styles.input}
+                value={person}
+                onChange={(e) => setPerson(e.target.value)}
+                type="text"
+              />
+              {isLoading ? (
+                <div>Hmmmm ...</div>
+              ) : (
+                <button className={styles.button} type="submit">
+                  Los geht's
+                </button>
+              )}
+            </>
+          )}
           {error && <p className={styles.error}>{error}</p>}
         </form>
+        <div className={styles.footer}>
+          Made with ❤️ by <a href="https://www.westhofen.me">westhofen.me</a>
+        </div>
       </div>
     </div>
   );
